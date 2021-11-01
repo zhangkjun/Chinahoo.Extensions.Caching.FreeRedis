9f9a740982a005c01ddddf4b8096005a78ffbca7
zhangkjun <zhangkjun@outlook.com>
2021-11-01 21:12:48 +0800

    add

---
+++ Chinahoo.Extensions.Caching.FreeRedis/FreeRedisDataProtectionBuilderExtensions.cs
@@ -0,0 +1,78 @@
+﻿using FreeRedis;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chinahoo.Extensions.Caching.FreeRedis
+{
+    /// <summary>
+    /// Contains Redis-specific extension methods for modifying a <see cref="IDataProtectionBuilder"/>.
+    /// </summary>
+    public static class FreeRedisDataProtectionBuilderExtensions
+    {
+        private const string DataProtectionKeysName = "DataProtection-Keys";
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to specified key in Redis database
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="databaseFactory">The delegate used to create <see cref="IDatabase"/> instances.</param>
+        /// <param name="key">The <see cref="RedisKey"/> used to store key list.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToFreeRedis(this IDataProtectionBuilder builder, Func<RedisClient> databaseFactory, string key)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (databaseFactory == null)
+            {
+                throw new ArgumentNullException(nameof(databaseFactory));
+            }
+            return PersistKeysToFreeRedisInternal(builder, databaseFactory, key);
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to the default key ('DataProtection-Keys') in Redis database
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="connectionMultiplexer">The <see cref="IConnectionMultiplexer"/> for database access.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToFreeRedis(this IDataProtectionBuilder builder, ConnectionStringBuilder connectionMultiplexer)
+        {
+            return PersistKeysToFreeRedis(builder, connectionMultiplexer, DataProtectionKeysName);
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys to the specified key in Redis database
+        /// </summary>
+        /// <param name="builder">The builder instance to modify.</param>
+        /// <param name="connectionMultiplexer">The <see cref="IConnectionMultiplexer"/> for database access.</param>
+        /// <param name="key">The <see cref="RedisKey"/> used to store key list.</param>
+        /// <returns>A reference to the <see cref="IDataProtectionBuilder" /> after this operation has completed.</returns>
+        public static IDataProtectionBuilder PersistKeysToFreeRedis(this IDataProtectionBuilder builder, ConnectionStringBuilder connectionMultiplexer, string key)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (connectionMultiplexer == null)
+            {
+                throw new ArgumentNullException(nameof(connectionMultiplexer));
+            }
+            return PersistKeysToFreeRedisInternal(builder, () => new RedisClient(connectionMultiplexer), key);
+        }
+
+        private static IDataProtectionBuilder PersistKeysToFreeRedisInternal(IDataProtectionBuilder builder, Func<RedisClient> databaseFactory, string key)
+        {
+            builder.Services.Configure<KeyManagementOptions>(options =>
+            {
+                options.XmlRepository = new RedisXmlRepository(databaseFactory, key);
+            });
+            return builder;
+        }
+    }
+}
---
+++ Chinahoo.Extensions.Caching.FreeRedis/RedisXmlRepository.cs
@@ -0,0 +1,52 @@
+﻿using FreeRedis;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Chinahoo.Extensions.Caching.FreeRedis
+{
+    /// <summary>
+    /// An XML repository backed by a Redis list entry.
+    /// </summary>
+    public class RedisXmlRepository : IXmlRepository
+    {
+        private readonly Func<RedisClient> _databaseFactory;
+        private readonly string _key;
+
+        /// <summary>
+        /// Creates a <see cref="RedisXmlRepository"/> with keys stored at the given directory.
+        /// </summary>
+        /// <param name="databaseFactory">The delegate used to create <see cref="IDatabase"/> instances.</param>
+        /// <param name="key">The <see cref="RedisKey"/> used to store key list.</param>
+        public RedisXmlRepository(Func<RedisClient> databaseFactory, string key)
+        {
+            _databaseFactory = databaseFactory;
+            _key = key;
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            return GetAllElementsCore().ToList().AsReadOnly();
+        }
+
+        private IEnumerable<XElement> GetAllElementsCore()
+        {
+            var database = _databaseFactory();
+            foreach (var value in database.LRange(_key, 0, -1))
+            {
+                yield return XElement.Parse(value);
+            }
+        }
+
+        /// <inheritdoc />
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            var database = _databaseFactory();
+            database.RPush(_key, element.ToString(SaveOptions.DisableFormatting));
+        }
+    }
+}
